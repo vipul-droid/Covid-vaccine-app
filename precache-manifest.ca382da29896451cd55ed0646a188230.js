77fff0349c037dfc73b71652180a28fcb7bb5cba
Vipul Tiwari <vipulengg98@gmail.com>
2021-11-15 13:36:47 +0530

    Updates

---
+++ precache-manifest.ca382da29896451cd55ed0646a188230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ef27cddc7ea89c93b62b451f13c0b3",
+    "url": "/Covid-vaccine-app/index.html"
+  },
+  {
+    "revision": "410fdf360be486d1c1e7",
+    "url": "/Covid-vaccine-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9f92c2b9047aca6ad9fa",
+    "url": "/Covid-vaccine-app/static/css/main.760fcc08.chunk.css"
+  },
+  {
+    "revision": "410fdf360be486d1c1e7",
+    "url": "/Covid-vaccine-app/static/js/2.3fb1dca2.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-vaccine-app/static/js/2.3fb1dca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f92c2b9047aca6ad9fa",
+    "url": "/Covid-vaccine-app/static/js/main.8b74cdf7.chunk.js"
+  },
+  {
+    "revision": "707ae9f36be6b0046cbe",
+    "url": "/Covid-vaccine-app/static/js/runtime-main.fedaf68f.js"
+  }
+]);
